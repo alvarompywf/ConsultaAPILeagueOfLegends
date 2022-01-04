c667fa8154313914d005e0650b6138a2b96b4e3b
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-01-04 13:52:56 +0100

    Añadido una imagen por detrás y arreglar cosas como imagen repetidas.

--- logica.js
+++ logica.js
@@ -17,38 +17,54 @@ function controlarSubmit(event) {
     event.preventDefault();
 
     var getInput = event.target.name.value;
-    getInput = getInput[0].toUpperCase() + getInput.slice(1);
+    var nombre = event.target.name.value;
+
+    nombre = getInput[0].toUpperCase() + getInput.slice(1);
+    getInput = getInput[0].toUpperCase() + getInput.slice(1);//poner la primera letra en mayusculas
 
     Object.entries(personajes).map(psj => {
 
         console.log(psj[1].name);
 
         if (getInput == psj[1].name) {
+
             var lore = psj[1].blurb;
             var ataque = psj[1].info.attack;
-             var defensa = psj[1].info.defense;
-             var dificultad = psj[1].info.difficulty;
-             var magia = psj[1].info.magic;
+            var defensa = psj[1].info.defense;
+            var dificultad = psj[1].info.difficulty;
+            var magia = psj[1].info.magic;
             
-            obtenerImagen(getInput, lore, ataque,defensa,dificultad,magia);
+            obtenerImagenAtras(nombre);
+            obtenerImagen(getInput, lore, ataque, defensa, dificultad, magia);
+            
         }
 
-
     });
-    
+
     console.log(getInput);
     //return getInput;
     console.log(personajes);
 }
 
-function obtenerImagen(nombrePersonaje, lore, ataque,defensa,dificultad,magia) {
+function obtenerImagen(nombrePersonaje, lore, ataque, defensa, dificultad, magia) {
 
 
     fetch(`http://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/${nombrePersonaje}.png`)
         .then(response => response.blob())
         .then(imageBlob => {
             const imageObjectURL = URL.createObjectURL(imageBlob);
-            crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificultad,magia);
+            crearDiv(imageObjectURL, nombrePersonaje, lore, ataque, defensa, dificultad, magia);
+        });
+
+}
+
+function obtenerImagenAtras(nombrePersonaje) {
+
+    fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${nombrePersonaje}_0.jpg`)
+        .then(response => response.blob())
+        .then(imageBlob2 => {
+            const imageObjectURL2 = URL.createObjectURL(imageBlob2);
+            cartaAtras(imageObjectURL2);
         });
 
 }
@@ -73,11 +89,11 @@ function resetPage() {
 }
 
 
-function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificultad,magia) {//DOM
+function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque, defensa, dificultad, magia) {//DOM
 
     resetPage();
 
-    var divPersonajes = document.querySelector("#personajes");//obteniendo el div del html donde pondremos el contenido.
+    var divPersonajes = document.getElementById("personajes");//obteniendo el div del html donde pondremos el contenido.
 
     var divCaja = document.createElement("div");//creamos un div
     divCaja.classList.add("caja");             //Y le añadimos una clase
@@ -89,7 +105,9 @@ function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificult
 
     p.appendChild(text);                              //le añadimos el texto al div
     divCaja.appendChild(p);                          //le añadimos un el parrafo al div
+
     divPersonajes.appendChild(divCaja);             //Y se lo añadimos 
+
 
     var img = document.createElement("img");     //creamos una etiqueta imagen <img></img>
     img.classList.add("imagen");                //le añadimos una clase.
@@ -108,7 +126,7 @@ function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificult
     //  DOM del ataque, defensa , ...
     var attackParr = document.createElement("p");
     attackParr.classList.add("atYDef");
-    
+
 
     var attackText = document.createTextNode("att: " + ataque);
 
@@ -116,13 +134,13 @@ function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificult
     divCaja.appendChild(attackParr);
     //defensa
     var defensaParr = document.createElement("p");
-    
+
 
     var defensaText = document.createTextNode("def: " + defensa);
     defensaParr.classList.add("atYDef");
     defensaParr.appendChild(defensaText);
     divCaja.appendChild(defensaParr);
-    
+
     //dificultad
     var dificultadParr = document.createElement("p");
     var dificultadText = document.createTextNode("dif: " + dificultad);
@@ -137,8 +155,39 @@ function crearDiv(imageObjectURL, nombrePersonaje, lore, ataque,defensa,dificult
     divCaja.appendChild(magiaParr);
 
 
-}
 
+}
+function resetPageAtras() {
+
+    var divPsjs = document.querySelector(".imagen2");
+
+    if (divPsjs != null) {// si hay algo se sustituye
+
+        divPsjs.remove();
+
+    }
+
+}
+function cartaAtras(imageObjectURL) {
+
+    
+    resetPageAtras();
+
+    
+    var divDetras = document.getElementById("atras");
+
+    var div2 = document.createElement("div"); 
+    div2.classList.add("divAtras");        
+    divDetras.appendChild(div2);
+
+
+    var img = document.createElement("img");     //creamos una etiqueta imagen <img></img>
+    img.classList.add("imagen2");                //le añadimos una clase.
+    img.setAttribute("src", imageObjectURL);   //y la ruta de la imagen SRC
+
+    divDetras.appendChild(img);
+
+}
 
 
 window.onload = obtenerPersonajes();
