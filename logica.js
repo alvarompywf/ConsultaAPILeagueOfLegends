5cd0bc3762f148fa742ef0ecda503b340ef97306
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-01-30 14:09:00 +0100

    funciona select e input

--- logica.js
+++ logica.js
@@ -2,14 +2,42 @@ var personajes;
 
 var sustituido = true;
 
+
+function agregarEventos() {
+
+    let select = document.querySelector("#selectPorNombre");
+    select.addEventListener("change", controlarSelect);
+    
+
+}
+
 async function obtenerPersonajes() {
+
     await fetch('http://ddragon.leagueoflegends.com/cdn/11.24.1/data/es_ES/champion.json')
         .then(response => response.json())
-        .then(data => personajes = data.data);
+        .then(data => {
+            personajes = data.data
+            console.log(personajes)
+        });
 
-console.log(personajes)
-        
-introducirEnElSelect();
+    introducirEnElSelect();
+    agregarEventos();
+}
+
+function introducirEnElSelect() {
+
+    //obtenemos el select vacio  
+    var selectPsjs = document.querySelector("#selectPorNombre");
+
+    Object.entries(personajes).map(psj => {//vamos rellenando el select creando options con todos los personajes 
+        var option = document.createElement("option");
+        option.setAttribute("id", psj[1].name);
+        option.setAttribute("name", psj[1].name);
+        option.setAttribute("value", psj[1].name);
+
+        option.innerText = psj[1].name;
+        selectPsjs.appendChild(option);// se rellena aquí 
+    });
 
 }
 
@@ -18,28 +46,37 @@ function controlarSubmit(event) {
     event.preventDefault();
 
     var getInput = event.target.name.value;
-    var nombre = event.target.name.value;
 
-    nombre = getInput[0].toUpperCase() + getInput.slice(1);
     getInput = getInput[0].toUpperCase() + getInput.slice(1);//poner la primera letra en mayusculas
 
     Object.entries(personajes).map(psj => {
 
-        console.log(psj[1].name);
-
         if (getInput == psj[1].name) {
-
-            obtenerImagenAtras(nombre);
             obtenerImagen(getInput);
-        
+        }
+    });
+
+}
+
+function controlarSelect(event) {
+    let select = document.querySelector("#selectPorNombre");
+    event.preventDefault();
+
+    var getSelect = this.options[select.selectedIndex].value;
+
+    Object.entries(personajes).map(psj => {
+
+        if (getSelect == psj[1].name) {
+
+            obtenerImagen(getSelect);
+
         }
 
     });
 
-    
 }
 
-function obtenerImagen(nombrePersonaje,) {
+function obtenerImagen(nombrePersonaje) {
 
     fetch(`http://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/${nombrePersonaje}.png`)
         .then(response => response.blob())
@@ -47,35 +84,6 @@ function obtenerImagen(nombrePersonaje,) {
             const imageObjectURL = URL.createObjectURL(imageBlob);
             crearDiv(imageObjectURL, nombrePersonaje);
         });
-
-}
-
-function obtenerImagenAtras(nombrePersonaje) {
-
-    fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${nombrePersonaje}_0.jpg`)
-        .then(response => response.blob())
-        .then(imageBlob2 => {
-            const imageObjectURL2 = URL.createObjectURL(imageBlob2);
-            cartaAtras(imageObjectURL2);
-        });
-}
-
-function borrar() {
-
-    var divPsjs = document.querySelector("#personajes div");
-    divPsjs.remove();
-
-}
-
-function resetPage() {
-
-    var divPsjs = document.querySelector("#personajes div");
-
-    if (divPsjs != null) {// si hay algo se sustituye
-
-        divPsjs.remove();
-
-    }
 
 }
 
@@ -99,17 +107,23 @@ function crearDiv(imageObjectURL, nombrePersonaje) {//DOM
 
     divPersonajes.appendChild(divCaja);             //Y se lo añadimos 
 
-
     var img = document.createElement("img");     //creamos una etiqueta imagen <img></img>
     img.classList.add("imagen");                //le añadimos una clase.
     img.setAttribute("src", imageObjectURL);   //y la ruta de la imagen SRC
 
     divCaja.appendChild(img);                       //se lo añadimos a la caja
+}
+
+function borrar() {
+
+    var divPsjs = document.querySelector("#personajes div");
+    divPsjs.remove();
 
 }
-function resetPageAtras() {
 
-    var divPsjs = document.querySelector(".imagen2");
+function resetPage() {
+
+    var divPsjs = document.querySelector("#personajes div");
 
     if (divPsjs != null) {// si hay algo se sustituye
 
@@ -117,43 +131,6 @@ function resetPageAtras() {
 
     }
 
-}
-function cartaAtras(imageObjectURL) {
-
-    
-    resetPageAtras();
-
-    
-    var divDetras = document.getElementById("atras");
-
-    var div2 = document.createElement("div"); 
-    div2.classList.add("divAtras");        
-    divDetras.appendChild(div2);
-
-
-    var img = document.createElement("img");     //creamos una etiqueta imagen <img></img>
-    img.classList.add("imagen2");                //le añadimos una clase.
-    img.setAttribute("src", imageObjectURL);   //y la ruta de la imagen SRC
-
-    divDetras.appendChild(img);
-
-}
-
-function introducirEnElSelect() {
-
-    //obtenemos el select vacio  
-
-    var selectPsjs = document.querySelector("#selectPorNombre");
-
-    console.log(personajes);
-    Object.entries(personajes).map(psj => {//vamos rellenando el select creando options con todos los personajes 
-        var option = document.createElement("option");
-        option.setAttribute("id", psj[1].name);
-        option.setAttribute("name", psj[1].name);
-        option.setAttribute("value", psj[1].name);
-        option.innerText = psj[1].name;
-        selectPsjs.appendChild(option);// se rellena aquí 
-    });
 }
 
 
